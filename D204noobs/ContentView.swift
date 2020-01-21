17f89133d68c9b0acc96db4bc9d4e316a7d35fd0
Mitchell Melrose <wmm18a@acu.edu>
2020-01-21 13:50:29 -0600

    yeet

---
+++ D204noobs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  D204noobs
+//
+//  Created by William Melrose on 1/21/20.
+//  Copyright © 2020 Mitchell Melrose. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! Yo!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
